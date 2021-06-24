a6dd3aa9358a3cca210582887b1187376b169d3b
Cameron Wolf <cameron.wolf@gsa.gov>
2021-06-24 12:21:31 -0400

    Initial commit of ngx-uswds-icons and basic demo site

---
+++ projects/icons/src/public-api.ts
@@ -0,0 +1,8 @@
+/*
+ * Public API Surface of icons
+ */
+
+export * from './lib/icon.component';
+export * from './lib/icon.module';
+export * from './lib/custom-icons';
+export * from './lib/uswds-icons';
---
+++ src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { IconModule } from 'projects/icons/src/public-api';
+import { NgxBootstrapIconsModule, allIcons } from 'ngx-bootstrap-icons';
+import { allIcons as sdsIcons } from 'projects/icons/src/public-api';
+import { uswdsAllIcons } from 'projects/icons/src/public-api';
+import { CommonModule } from '@angular/common';
+import * as _ from 'lodash';
+
+export const appendPrefix = (iconsObject, prefix): Object => {
+  const a = {};
+  Object.keys(iconsObject).forEach(key => {
+    a[`${prefix}${_.upperFirst(key)}`]=iconsObject[key];
+  })
+  return a;
+};
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    IconModule,
+    NgxBootstrapIconsModule.pick(Object.assign(
+      {},
+      _.cloneDeep(allIcons),
+      appendPrefix(_.cloneDeep(sdsIcons), 'sds'),
+      _.cloneDeep(uswdsAllIcons),
+    )),
+    CommonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
