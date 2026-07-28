3329e88a644b9428697d93ad33d176638bb43759
Frank Pigeon Jr. <frank.pigeon@gsa.gov>
2026-07-28 09:08:39 -0500

    ci: upload Vitest coverage artifact (#53)
    * ci: upload Vitest coverage artifact
    
    * Address PR review feedback
    
    * Include full coverage artifact
    
    * fix: rename stacked-icon.modue.ts to stacked-icon.module.ts

--- projects/icons/src/public-api.ts
+++ projects/icons/src/public-api.ts
@@ -5,6 +5,6 @@
 export * from './lib/icon/icon.component';
 export * from './lib/icon/icon.module';
 export * from './lib/stacked-icon/stacked-icon.component';
-export * from './lib/stacked-icon/stacked-icon.modue';
+export * from './lib/stacked-icon/stacked-icon.module';
 export * from './lib/custom-icons';
 export * from './lib/uswds-icons';
